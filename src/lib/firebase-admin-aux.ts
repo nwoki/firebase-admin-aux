c8959945570802c51508c48638ff3538ca45ceee
Francesco Nwokeka <francesco.nwokeka@gmail.com>
2023-03-27 18:01:14 +0200

    basic TS setup configured with new redis connection established

---
+++ src/lib/firebase-admin-aux.ts
@@ -0,0 +1,284 @@
+import admin from 'firebase-admin';
+import { App, initializeApp } from 'firebase-admin/app';
+import { getMessaging, Messaging } from 'firebase-admin/messaging';
+import { RedisConnection } from './redisconnection';
+import UrlParse from 'url-parse';
+
+export interface FirebaseAccountConfig {
+    name: string,               // key to identify the configuration with
+    jsonCredentials: string     // json credentials of our firebase application
+}
+
+export interface FirebaseAdminObj {
+    admin: App,
+    messaging: Messaging
+}
+
+export class FirebaseAdminAux {
+    /**
+     * key: firebase config name
+     * value: firebase admin obj
+     */
+    private m_firebaseAccounts = new Map<string, FirebaseAdminObj>;
+    private m_initialized: boolean = false;
+    private m_redisConnection: RedisConnection;// = undefined;// = new RedisConnection();
+
+    constructor(withCache?: boolean, redisUrl?: string) {
+        if (withCache) {
+            if (!redisUrl) {
+                redisUrl = process.env.REDIS_CACHE_URL ?? process.env.REDIS_URL as string ?? 'redis://localhost:6379';
+            }
+
+            // await setupRedisCache();
+            this.m_redisConnection = new RedisConnection(redisUrl, 'FirebaseAdminAux');
+        }
+    }
+
+    public async init(configs: FirebaseAccountConfig[]) {
+        if (this.m_initialized) {
+            console.log('FirebaseAdminAux already initialized!');
+            return;
+        }
+
+        for (const config of configs) {
+            // don't overwrite configuration that's already been initialized
+            if (this.m_firebaseAccounts.get(config.name)) {
+                continue;
+            }
+
+            const firebaseAdmin = initializeApp({
+                credential: admin.credential.cert(JSON.parse(config.jsonCredentials))
+            }, config.name);
+            const firebaseMessaging = getMessaging(firebaseAdmin);
+
+            // stash
+            this.m_firebaseAccounts.set(config.name, {admin: firebaseAdmin, messaging: firebaseMessaging} as FirebaseAdminObj);
+        }
+
+        this.m_initialized = true;
+
+        if (this.m_redisConnection) {
+            await this.m_redisConnection.init();
+        }
+    }
+
+    /**
+     * @return FirebaseAdminObj given a config name
+     */
+    public account(configName: string): FirebaseAdminObj | undefined {
+        return this.m_firebaseAccounts.get(configName);
+    }
+}
+
+
+
+// NOTE: add `FirebaseAdminAux` ad default export?
+
+
+
+
+// const FirebaseAdmin = require("firebase-admin");
+// const chalk = require("chalk");
+// const JSONAPIError = require('jsonapi-serializer').Error;
+//
+//
+// function customError(code, title, message) {
+//     return new JSONAPIError({
+//         status: code,
+//         title: title,
+//         detail: message
+//     });
+// };
+//
+// function firebaseError(error) {
+//     let code = 400;
+//
+//     if (error.code == "auth/id-token-expired" || error.code == "auth/argument-error"
+//         || error.code == "auth/invalid-user-token" || error.code == "auth/user-token-expired") {
+//         code = 401;
+//     }
+//
+//     return new JSONAPIError({
+//         status: code,
+//         title: error.code,
+//         detail: error.message
+//     });
+// };
+//
+// let FirebaseAdminAux = (function () {
+//
+//
+//     function populateResponseObject(responseObject, decodedTokenObj, bearerToken) {
+//         responseObject.locals.firebase_uid = decodedTokenObj.user_id;
+//         responseObject.locals.decoded_token = decodedTokenObj;
+//         responseObject.locals.bearer_token = bearerToken;
+//     }
+//
+//     function requestVerification(bearerToken, res) {
+//         return new Promise((resolve, reject) => {
+//             app.auth().verifyIdToken(bearerToken).then((decodedToken) => {
+//                 console.log("[FirebaseAdminAux] %s", JSON.stringify(decodedToken));
+//
+//                 // Stash the token if redis cache is active
+//                 if (redisCache) {
+//                     redisAuxClient.setWithExpiry("firebase_aux_" + bearerToken, JSON.stringify(decodedToken), (decodedToken.exp - Math.round(new Date() / 1000))).then(() => {
+//                         resolve(decodedToken);
+//                     }).catch((error) => {
+//                         console.error(error);
+//                         reject(error);
+//                     });
+//                 } else {
+//                     resolve(decodedToken);
+//                 }
+//             }).catch((error) => {
+//                 console.error(error.toString());
+//                 let datError = firebaseError(error);
+//                 /*return */res.status(datError.errors[0].status).send(datError);
+//                 reject();
+//             });
+//         });
+//     }
+//     /**************************************************************/
+//
+//     function createUser(userEmail, userPassword) {
+//         return new Promise((resolve, reject) => {
+//             app.auth().createUser({
+//                 email: userEmail,
+//                 password: userPassword
+//             }).then((userRecord) => {
+//                 // See the UserRecord reference doc for the contents of userRecord.
+//                 console.log("[FirabaseAdminAuth] Successfully created new user: %s", userRecord.uid);
+//                 resolve(userRecord);
+//             }).catch((error) => {
+//                 reject(error);
+//             });
+//         });
+//     }
+//
+//     function deleteUser(firebaseUid) {
+//         return new Promise((resolve, reject) => {
+//             app.auth().deleteUser(firebaseUid)
+//             .then(() => {
+//                 resolve();
+//             }).catch((error) => {
+//                 reject(error);
+//             });
+//         });
+//     };
+//
+//     function sendPushNotification(title, body, registrationToken) {
+//         let msg = {
+//             message: {
+//                 notification: {
+//                     title: title,
+//                     body: body
+//                 },
+//                 token: registrationToken
+//             }
+//         };
+//
+//         app.messaging().send(msg)
+//         .then((response) => {
+//             // Response is a message ID string.
+//             console.log('Successfully sent message:', response);
+//         })
+//         .catch((error) => {
+//             console.log('Error sending message:', error);
+//         });
+//     };
+//
+//     function sendMultiplePushNotifications(title, body, registrationTokens) {
+//         let msg = {
+//             message: {
+//                 notification: {
+//                     title: title,
+//                     body: body
+//                 },
+//                 tokens: registrationTokens
+//             }
+//         };
+//
+//         app.messaging().sendMulticast(msg)
+//         .then((response) => {
+//             // Response is a message ID string.
+//             console.log('Successfully sent message:', response);
+//         })
+//         .catch((error) => {
+//             console.log('Error sending message:', error);
+//         });
+//     };
+//
+//     function userExists(userEmail) {
+//         return new Promise((resolve, reject) => {
+//             if (!app) {
+//                 reject(new Error("[FirabaseAdminAuth] You must initialize the module first!"));
+//             } else {
+//                 app.auth().getUserByEmail(userEmail).then((user) => {
+//                     resolve(user);
+//                 }).catch((error) => {
+//                     reject(error);
+//                 });
+//             }
+//         });
+//     };
+//
+//     var validateToken = (req, res, next) => {
+//         if (!req.headers.authorization) {
+//             return res.status(401).send(customError(401, "Unauthorized", "Missing authorization"));
+//         }
+//
+//         let split = req.headers.authorization.split(' ');
+//
+//         if (split.length < 2) {
+//             return res.status(400).send(customError(400, "Bad request", "Malformed authorization"));
+//         }
+//
+//         let bearerToken = req.headers.authorization.split(' ')[1];
+//
+//         if (bearerToken) {
+//             // check if the token is already stashed
+//             if (redisCache) {
+//                 redisAuxClient.get("firebase_aux_" + bearerToken).then((decodedToken) => {
+//                     if (decodedToken) {
+//                         let decodedObj = JSON.parse(decodedToken);
+//
+//                         // token is present, already verified and has yet to expire
+//                         populateResponseObject(res, decodedObj, bearerToken);
+//                         next();
+//                     } else {
+//                         console.log("Token has expired! Request new verification");
+//                         requestVerification(bearerToken, res).then((decodedToken) => {
+//                             populateResponseObject(res, decodedToken, bearerToken);
+//                             next();
+//                         }).catch((firebaseError) => {
+//                             console.error(firebaseError);
+//                         });
+//                     }
+//                 }).catch((error) => {
+//                     console.error(error);
+//                 });
+//             } else {
+//                 requestVerification(bearerToken, res).then((decodedToken) => {
+//                     populateResponseObject(res, decodedToken, bearerToken);
+//                     next();
+//                 }).catch((error) => {
+//                     console.error(error);
+//                 });
+//             }
+//         } else {
+//             return res.status(400).send(customError(400, "Bad request", "Missing auth token"));
+//         }
+//     }
+//
+//     return {
+//         createUser: createUser,
+//         deleteUser: deleteUser,
+//         sendPushNotification: sendPushNotification,
+//         sendMultiplePushNotifications: sendMultiplePushNotifications,
+//         userExists: userExists,
+//         validateToken: validateToken,
+//         initialize
+//     }
+// })();
+//
+// module.exports = FirebaseAdminAux;
---
+++ src/lib/redisconnection.ts
@@ -0,0 +1,138 @@
+// import { Logger } from '@rawfish-stiga/logger';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+import Redis from 'ioredis';
+import UrlParse from 'url-parse';
+
+
+// TODO : make this a sperate module / installable package
+
+
+// NOTE: no need to export it at the moment
+/*export */function getRedisOption(url?: string, name?: string) {
+    if (!url) {
+        url = process.env.REDIS_URL_QUEUE ?? process.env.REDIS_URL as string;
+    }
+    const connName = name ?? 'Redis ' + url;
+    const connNameLog = '[' + connName + '] ';
+    // Logger.info(connNameLog + 'init');
+
+    const urlParse = new UrlParse(url);
+    const redisOptions = {
+        connName,
+        connNameLog,
+        host: urlParse.hostname,
+        port: parseInt(urlParse.port, 10),
+        protocol: urlParse.protocol,
+        retry_strategy(options: {attempt: number}) {
+            // Logger.error(connNameLog + `going to reconnect after: ${Math.min(options.attempt * 100, 3000)}`);
+            return Math.min(options.attempt * 100, 3000);
+        }
+    };
+    return redisOptions;
+}
+
+export class RedisConnection {
+    private m_redisClient;
+    public readonly connName: string;
+    private m_isReady: boolean = false;
+
+    constructor(url?: string, name?: string) {
+        const options = getRedisOption(url, name);
+        this.connName = options.connName;
+
+        this.m_redisClient = new Redis(options);
+        this.m_redisClient.on('ready', () => {
+            // /*Logger*/.info(options.connNameLog + 'ready');
+            this.m_isReady = true;
+        });
+    }
+
+    public async init(): Promise<void> {
+        if (this.m_isReady) return;
+        return new Promise<void>((resolve) => {
+            this.m_redisClient.on('ready', () => {
+                resolve();
+            });
+        });
+    }
+
+    /**
+     * return redis client (you must have executed connect() before)
+     * @returns
+     */
+    public client() {
+        return this.m_redisClient;
+    }
+
+    public keys(pattern: string) {
+        return new Promise<any>((resolve, reject) => {
+            this.m_redisClient.keys(pattern, (error: any, result: any) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    public set(key: string, value: any) {
+        return new Promise<void>((resolve, reject) => {
+            this.m_redisClient.set(key, value, (error: any) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    public setWithExpiry(key: string, value: any, expireSeconds: number) {
+        return new Promise<void>((resolve, reject) => {
+            this.m_redisClient.set(key, value, 'EX', expireSeconds, (error: any) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    public get(key: string) {
+        return new Promise<any>((resolve, reject) => {
+            this.m_redisClient.get(key, (error: any, result: any) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    public del(key: string) {
+        return new Promise<void>((resolve, reject) => {
+            this.m_redisClient.del(key, (error: any) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    public flush() {
+        return new Promise<void>((resolve, reject) => {
+            this.m_redisClient.flushall((error: any) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+}
